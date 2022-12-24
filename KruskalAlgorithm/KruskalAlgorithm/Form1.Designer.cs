3c243689a867207f0003b31c918951e251a3812f
ahmedsaleh210 <96204940+ahmedsaleh210@users.noreply.github.com>
2022-12-24 10:14:04 +0200

    Add files via upload
    Initial Commit

---
+++ KruskalAlgorithm/KruskalAlgorithm/Form1.Designer.cs
@@ -0,0 +1,177 @@
+﻿namespace KruskalAlgorithm
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.verticesNumber = new System.Windows.Forms.TextBox();
+            this.edgesNumber = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.edgesField = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // verticesNumber
+            // 
+            this.verticesNumber.Location = new System.Drawing.Point(54, 51);
+            this.verticesNumber.Name = "verticesNumber";
+            this.verticesNumber.Size = new System.Drawing.Size(161, 26);
+            this.verticesNumber.TabIndex = 0;
+            this.verticesNumber.TextChanged += new System.EventHandler(this.verticesNumber_TextChanged);
+            // 
+            // edgesNumber
+            // 
+            this.edgesNumber.Location = new System.Drawing.Point(261, 51);
+            this.edgesNumber.Name = "edgesNumber";
+            this.edgesNumber.Size = new System.Drawing.Size(158, 26);
+            this.edgesNumber.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(50, 28);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(129, 20);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "No. of vertices:";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(257, 28);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(116, 20);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "No. of edges:";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(50, 95);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(304, 20);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Enter the edges (comma-separated):";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // edgesField
+            // 
+            this.edgesField.Location = new System.Drawing.Point(57, 118);
+            this.edgesField.Name = "edgesField";
+            this.edgesField.Size = new System.Drawing.Size(362, 26);
+            this.edgesField.TabIndex = 5;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(158, 176);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(154, 44);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Kruskal Algorithm";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(98, 258);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.ReadOnly = true;
+            this.richTextBox1.Size = new System.Drawing.Size(289, 87);
+            this.richTextBox1.TabIndex = 7;
+            this.richTextBox1.Text = "";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(94, 235);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(206, 20);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Minimum Spanning Tree:";
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("MS Reference Sans Serif", 7F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.ForeColor = System.Drawing.SystemColors.ButtonShadow;
+            this.label5.Location = new System.Drawing.Point(58, 147);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(242, 18);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "Ex: 0 1 3,0 3 5,1 2 1,2 3 8,3 2 4";
+            this.label5.Click += new System.EventHandler(this.label5_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(495, 374);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.edgesField);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.edgesNumber);
+            this.Controls.Add(this.verticesNumber);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox verticesNumber;
+        private System.Windows.Forms.TextBox edgesNumber;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox edgesField;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+    }
+}
+
---
+++ KruskalAlgorithm/KruskalAlgorithm/Form1.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace KruskalAlgorithm
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void verticesNumber_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Clear();
+            int numVertices = int.Parse(verticesNumber.Text);
+            int numEdges = int.Parse(edgesNumber.Text);
+            string[] inputString = edgesField.Text.Split(',');
+            List<Tuple<int, int, int>> edges = new List<Tuple<int, int, int>>();
+            for (int n = 0; n < inputString.Length; n++)
+            {
+                string[] edgeString = inputString[n].Split(' ');
+                int u = int.Parse(edgeString[0]);
+                int v = int.Parse(edgeString[1]);
+                int w = int.Parse(edgeString[2]);
+                edges.Add(new Tuple<int, int, int>(w, u, v));
+            }
+            edges.Sort();
+
+            int[] parent = new int[numVertices];
+            for (int j = 0; j < numVertices; j++)
+            {
+                parent[j] = j;
+            }
+
+            List<Tuple<int, int>> mst = new List<Tuple<int, int>>();
+            int i = 0;
+            while (mst.Count < numVertices - 1)
+            {
+                int u = edges[i].Item2;
+                int v = edges[i].Item3;
+                int w = edges[i].Item1;
+                i++;
+                int setU = Find(parent, u);
+                int setV = Find(parent, v);
+                if (setU != setV)
+                {
+                    mst.Add(new Tuple<int, int>(u, v));
+                    Union(parent, setU, setV);
+                }
+            }
+            foreach (Tuple<int, int> edge in mst)
+            {
+                richTextBox1.AppendText(edge.Item1 + " - " + edge.Item2 + "\n");
+            }
+        }
+
+        static int Find(int[] parent, int vertex)
+        {
+            if (parent[vertex] != vertex)
+            {
+                parent[vertex] = Find(parent, parent[vertex]);
+            }
+            return parent[vertex];
+        }
+
+        // Union two disjoint sets
+        static void Union(int[] parent, int set1, int set2)
+        {
+            parent[set1] = set2;
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+
+}
+
+
